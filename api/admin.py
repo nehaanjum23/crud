169ab987a4db8bc6049a726dd18e6600f904136d
neha <nehaanjum2309@gmail.com>
2023-08-31 05:17:04 -0700

    CRUD operation using custom

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import student
+from .models import StudentDetail
 # Register your models here.
-@admin.register(student)
-class studentAdmin(admin.ModelAdmin):
-    list_display=['id','name','roll','city']
+@admin.register(StudentDetail)
+class StudentAdmin(admin.ModelAdmin):
+    list_display=['id','first_name','last_name','age']
